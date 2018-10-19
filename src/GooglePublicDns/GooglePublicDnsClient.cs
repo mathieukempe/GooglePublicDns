44abc7bc59bd57b0e113fafe5dfc9005d128a10b
Carlos Boeing <carlosboeing@gmail.com>
2018-10-19 16:14:05 +1100

    Change HttpClient to static variable

--- src/GooglePublicDns/GooglePublicDnsClient.cs
+++ src/GooglePublicDns/GooglePublicDnsClient.cs
@@ -7,10 +7,23 @@ namespace GooglePublicDns
 {
     public class GooglePublicDnsClient
     {
-        public const string BaseUrl = "https://dns.google.com/resolve?";
+        private static HttpClient _client;
 
         /// <summary>
-        /// 
+        /// Instantiate client
+        /// </summary>
+        /// <param name="timeout">Timespan to wait before the request times out (miliseconds)</param>
+        public GooglePublicDnsClient(long timeout = 1000)
+        {
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri("https://dns.google.com/"),
+                Timeout = TimeSpan.FromMilliseconds(timeout)
+            };
+        }
+
+        /// <summary>
+        /// Resolve DNS entry
         /// </summary>
         /// <param name="name"></param>
         /// <param name="type"></param>
@@ -20,41 +33,47 @@ namespace GooglePublicDns
         /// <returns></returns>
         public async Task<GooglePublicDnsResponse> Resolve(string name, RecordType? type = null, bool cd = false, string ednsClientSubnet = "", string randomPadding = "")
         {
-            var client = new HttpClient();
-
-            var query = "name=" + Uri.EscapeDataString(name);
-
-            if (type.HasValue)
-                query += "&type=" + type;
-
-            if (cd)
-                query += "&cd=true";
-
-            if (!string.IsNullOrEmpty(ednsClientSubnet))
+            try
             {
-                query += "&edns_client_subnet=" + ednsClientSubnet;
-            }
+                var query = "name=" + Uri.EscapeDataString(name);
 
-            if (!string.IsNullOrEmpty(randomPadding))
+                if (type.HasValue)
+                    query += "&type=" + type;
+
+                if (cd)
+                    query += "&cd=true";
+
+                if (!string.IsNullOrEmpty(ednsClientSubnet))
+                {
+                    query += "&edns_client_subnet=" + ednsClientSubnet;
+                }
+
+                if (!string.IsNullOrEmpty(randomPadding))
+                {
+                    query += "&random_padding=" + randomPadding;
+                }
+
+                HttpResponseMessage response = await _client.GetAsync($"resolve?{query}").ConfigureAwait(false);
+
+                var body = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                response.EnsureSuccessStatusCode();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var dnsResponse = JsonConvert.DeserializeObject<GooglePublicDnsResponse>(body);
+                    dnsResponse.RawJson = body;
+
+                    return dnsResponse;
+                }
+
+                return null;
+            }
+            catch (TaskCanceledException)
             {
-                query += "&random_padding=" + randomPadding;
+                // timeout
+                throw new TimeoutException();
             }
-
-            HttpResponseMessage response = await client.GetAsync(BaseUrl + query).ConfigureAwait(false);
-
-            var body = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-
-            response.EnsureSuccessStatusCode();
-
-            if (response.IsSuccessStatusCode)
-            {
-                var dnsResponse = JsonConvert.DeserializeObject<GooglePublicDnsResponse>(body);
-                dnsResponse.RawJson = body;
-
-                return dnsResponse;
-            }
-
-            return null;
         }
     }
 }
